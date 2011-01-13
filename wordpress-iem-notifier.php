0872d561144e69b0c6b0899b9f7a6fdc0f96b748
Jorge Falcão <jlbfalcao@gmail.com>
2011-01-13 19:16:10 -0200

    changing functions

--- wordpress-iem-notifier.php
+++ wordpress-iem-notifier.php
@@ -13,22 +13,25 @@ class HolmesNotify {
 	function HolmesNotify() {
 		$this->host = '192.168.42.94';
 		$this->queue = '/queue/foo';
-		
-		// add_action('publish_post ', array(&$this, 'publish_post'));
-		// add_action('comment_post ', array(&$this, 'comment_post'));
-		// add_action('wp_login ', array(&$this, 'wp_login'));
-		// add_action('wp_logout ', array(&$this, 'wp_logout'));
+		if (function_exists('add_action') ) {
+			add_action('publish_post ', array(&$this, 'publish_post'));
+			add_action('comment_post ', array(&$this, 'comment_post'));
+			add_action('wp_login ', array(&$this, 'wp_login'));
+			add_action('wp_logout ', array(&$this, 'wp_logout'));
+		}
 	}
 	
-	function send_stomp($data) {
+	function send_stomp($data, $type = '') {
 		$port = 61613;
 		$queue = $this->queue;
 		$host = $this->host;
+		$timestamp = (int)microtime(true);
 		
 		// echo $queue;
 		
 		$msg = "CONNECT\n\n\x00";
-		$msg .= "SEND\ndestination:$queue\ntimestamp:1\n\n$data\x00";
+		// add $type.
+		$msg .= "SEND\ndestination:$queue\ntimestamp:$timestamp\n\n$data\x00";
 		$msg .= "DISCONNECT\n\n\x00";
 
 		$l = fsockopen($host, $port, $errno, $errstr, 1);
@@ -70,24 +73,31 @@ class HolmesNotify {
 		return true;
 	}
 
-	function publish_post() {
-		$this->send_stomp('{title: "alo-você"}');
+	/**
+	 * When a post was published
+	 */
+	function publish_post($post_ID) {
+		// #TODO: add blog_ID. IDEA: get globals.
+		$this->send_stomp("{post_ID: ${post_ID}}", 'publish_post');
 	}
 
-	function comment_post() {
-		$this->send_stomp('{title: "alo-você"}');
+	function comment_post($comment_id) {
+		// #TODO: get blog_id and post_id
+		$this->send_stomp("{comment_id: ${comment_id}}", 'comment_post');
 	}
 
 	function wp_logout() {
-		$this->send_stomp('{title: "alo-você"}');
+		// TODO: get current logged user.
+		// $this->send_stomp('{data: ""}');
 	}
 
-	function wp_login() {
-		$this->send_stomp('{title: "alo-você"}');
+	function wp_login($login) {
+		$this->send_stomp("{login: '${login}'}", 'wp_login');
 	}
 }
 $holmes_notify = new HolmesNotify();
-$holmes_notify->wp_login();
+// if you want test.
+$holmes_notify->wp_login('josé');
 
 
 
